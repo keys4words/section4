bbecae034e2127325badb6852ac8077cb11ca1ea
keys4words <keys4words@gmail.com>
2020-01-23 16:59:39 +0300

    Add test for login_page

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
 	LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+
+class LoginPageLocators:
+	LOGIN_FORM = (By.CSS_SELECTOR, 'form#login_form')
+	REGISTER_FORM = (By.CSS_SELECTOR, 'form#register_form')
