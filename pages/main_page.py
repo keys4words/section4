ea92c80ffcd58774ec8b4a48cc225b58ddc309f3
Max 19038 <keys4words@gmail.com>
2020-01-26 21:34:45 +0300

    Add explicit transfer

--- pages/main_page.py
+++ pages/main_page.py
@@ -11,4 +11,5 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
-        return LoginPage(browser=self.browser, url=self.browser.current_url)
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
+        
